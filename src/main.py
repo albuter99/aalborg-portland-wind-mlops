45e49cd7516042f2c760a9a0703b5afead6db932
Alvaro Buendia <alvaro99b@gmail.com>
2026-05-14 18:12:04 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
+from fetch_weather import main as fetch_weather_main
+from generate_demand import main as generate_demand_main
+from simulate_turbines import main as simulate_turbines_main
+from analyze_results import main as analyze_results_main
 
+
+if __name__ == "__main__":
+    fetch_weather_main()
+    generate_demand_main()
+    simulate_turbines_main()
+    analyze_results_main()
+
+    print("Full pipeline completed successfully")
